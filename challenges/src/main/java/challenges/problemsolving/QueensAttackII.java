ccfe699aecc083a61cb9af3034b37333ed9c6b86
Leonardo Meinerz Ramos <leonardo.meinerzramos28@gmail.com>
2019-12-06 23:20:41 -0200

    resolução QueensAttack

--- challenges/src/main/java/challenges/problemsolving/QueensAttackII.java
+++ challenges/src/main/java/challenges/problemsolving/QueensAttackII.java
@@ -8,21 +8,47 @@ public class QueensAttackII {
         int obstaculoAbaixo = 0;
         int obstaculoAcima = n + 1;
 
+        int espaco_superior_direito = Integer.min(n - linhaRainha, n - colunaRainha);
+        int espaco_superior_esquerdo = Integer.min(n - linhaRainha, colunaRainha - 1);
+        int espaco_inferior_esquerdo = Integer.min(linhaRainha - 1, colunaRainha - 1);
+        int espaco_inferior_direito = Integer.min(n - colunaRainha, linhaRainha - 1);
+
         for (int[] obstacle : obstacles) {
-            if (obstacle[0] == linhaRainha) {
-                if (obstacle[1] < colunaRainha) {
-                    obstaculoEsquerda = Integer.max(obstacle[1], obstaculoEsquerda);
-                } else if (obstacle[1] > colunaRainha) {
-                    obstaculoDireita = Integer.min(obstacle[1], obstaculoDireita);
+            int linha = obstacle[0];
+            int coluna = obstacle[1];
+            if (linha == linhaRainha) {
+                if (coluna < colunaRainha) {
+                    obstaculoEsquerda = Integer.max(coluna, obstaculoEsquerda);
+                } else if (coluna > colunaRainha) {
+                    obstaculoDireita = Integer.min(coluna, obstaculoDireita);
+                }
+            } else if (coluna == colunaRainha) {
+                if (linha < linhaRainha) {
+                    obstaculoAbaixo = Integer.max(linha, obstaculoAbaixo);
+                } else if (linha > linhaRainha) {
+                    obstaculoAcima = Integer.min(linha, obstaculoAcima);
+                }
+            } else if (linha - linhaRainha == coluna - colunaRainha) {
+                if (linha - linhaRainha > 0) {
+                    espaco_superior_direito = Integer.min(espaco_superior_direito, linha - linhaRainha - 1);
+                } else if (linha - linhaRainha < 0) {
+                    espaco_inferior_esquerdo = Integer.min(espaco_inferior_esquerdo, linhaRainha - linha - 1);
+                }
+            } else if (coluna - colunaRainha == -(linha - linhaRainha)) {
+                if (linha - linhaRainha > 0) {
+                    espaco_superior_esquerdo = Integer.min(espaco_superior_esquerdo, linha - linhaRainha - 1);
+                } else if (linha - linhaRainha < 0) {
+                    espaco_inferior_direito = Integer.min(espaco_inferior_direito, linhaRainha - linha - 1);
                 }
             }
         }
 
         int livreAEsquerda = colunaRainha - obstaculoEsquerda - 1;
-        int livreADireita = obstaculoDireita - colunaRainha;
-        int livreAcima = obstaculoAcima - colunaRainha;
-        int livraAbaixo = obstaculoDireita - colunaRainha;
-        return livreAEsquerda + livreADireita;
+        int livreADireita = obstaculoDireita - colunaRainha - 1;
+        int livreAcima = obstaculoAcima - linhaRainha - 1;
+        int livreAbaixo = linhaRainha - obstaculoAbaixo - 1;
+        return livreAEsquerda + livreADireita + livreAcima + livreAbaixo + espaco_superior_direito
+                + espaco_inferior_esquerdo + espaco_superior_esquerdo + espaco_inferior_direito;
     }
 
 
--- challenges/src/test/java/challenges/problemsolving/QueensAttackIITest.java
+++ challenges/src/test/java/challenges/problemsolving/QueensAttackIITest.java
@@ -14,12 +14,319 @@ class QueensAttackIITest {
         assertEquals(9, QueensAttackII.queensAttack(4, 0, 4, 4, obstacles));
     }
 
-    @Test
+    // @Test
     public void sampleOne() {
 
-        int[][] obstacles = new int[][]{{4,1}};
+        int[][] obstacles = new int[][]{{4, 2}, {5, 5}, {2, 3}};
 
-        assertEquals(9, QueensAttackII.queensAttack(5, 1, 4, 3, obstacles));
+        assertEquals(10, QueensAttackII.queensAttack(5, 3, 4, 3, obstacles));
     }
+
+    @Test
+    public void diagonalSimpleTest() {
+
+        int[][] obstacles = new int[][]{};
+
+        assertEquals(9, QueensAttackII.queensAttack(4, 0, 1, 1, obstacles));
+    }
+
+
+    @Test
+    public void diagonalWithObstacle() {
+
+        int[][] obstacles = new int[][]{{3, 3}};
+
+        assertEquals(7, QueensAttackII.queensAttack(4, 1, 1, 1, obstacles));
+    }
+
+    @Test
+    public void diagonalWithObstacle2() {
+
+        int[][] obstacles = new int[][]{{4, 4}};
+
+        assertEquals(8, QueensAttackII.queensAttack(4, 1, 1, 1, obstacles));
+    }
+
+    @Test
+    public void diagonalWithObstacle3() {
+
+        int[][] obstacles = new int[][]{};
+
+        assertEquals(9, QueensAttackII.queensAttack(4, 0, 4, 4, obstacles));
+    }
+
+    @Test
+    public void explanation1() {
+        int[][] obstacles = new int[][]{{5, 5}, {4, 2}, {2, 3}};
+
+        assertEquals(10, QueensAttackII.queensAttack(5, 3, 4, 3, obstacles));
+
+    }
+
+    @Test
+    public void bigExample2() {
+        int[][] obstacles = new int[][]{{3, 5}};
+
+        assertEquals(24, QueensAttackII.queensAttack(8, 1, 4, 4, obstacles));
+
+    }
+
+    @Test
+    public void bigExample1() {
+        int[][] obstacles = new int[][]{};
+
+        assertEquals(27, QueensAttackII.queensAttack(8, 0, 4, 4, obstacles));
+
+    }
+
+    @Test
+    public void teste1() {
+        int[][] obstacles = new int[][]{};
+
+        assertEquals(14, QueensAttackII.queensAttack(5, 0, 4, 3, obstacles));
+
+    }
+
+    @Test
+    public void teste2() {
+        int[][] obstacles = new int[][]{{4, 1}};
+
+        assertEquals(13, QueensAttackII.queensAttack(5, 1, 4, 3, obstacles));
+
+    }
+
+    @Test
+    public void teste3() {
+        int[][] obstacles = new int[][]{{4, 2}};
+
+        assertEquals(12, QueensAttackII.queensAttack(5, 1, 4, 3, obstacles));
+
+    }
+
+    @Test
+    public void teste4() {
+        int[][] obstacles = new int[][]{{4, 1}, {4, 2}};
+
+        assertEquals(12, QueensAttackII.queensAttack(5, obstacles.length, 4, 3, obstacles));
+
+    }
+
+
+    @Test
+    public void teste5() {
+        int[][] obstacles = new int[][]{{4, 2}, {4, 1}};
+
+        assertEquals(12, QueensAttackII.queensAttack(5, obstacles.length, 4, 3, obstacles));
+
+    }
+
+
+    @Test
+    public void teste6() {
+        int[][] obstacles = new int[][]{{5, 1}};
+
+        assertEquals(14, QueensAttackII.queensAttack(5, obstacles.length, 4, 3, obstacles));
+
+    }
+
+
+    @Test
+    public void teste7() {
+        int[][] obstacles = new int[][]{{4, 4}};
+
+        assertEquals(12, QueensAttackII.queensAttack(5, obstacles.length, 4, 3, obstacles));
+
+    }
+
+    @Test
+    public void teste8() {
+        int[][] obstacles = new int[][]{{4, 4}, {4, 2}};
+
+        assertEquals(10, QueensAttackII.queensAttack(5, obstacles.length, 4, 3, obstacles));
+
+    }
+
+    @Test
+    public void teste9() {
+        int[][] obstacles = new int[][]{{4, 5}, {4, 2}};
+
+        assertEquals(11, QueensAttackII.queensAttack(5, obstacles.length, 4, 3, obstacles));
+
+    }
+
+    @Test
+    public void teste10() {
+        int[][] obstacles = new int[][]{{3, 1}, {3, 5}, {5, 3}, {1, 3}};
+
+        assertEquals(12, QueensAttackII.queensAttack(5, obstacles.length, 3, 3, obstacles));
+
+    }
+
+    @Test
+    public void teste11() {
+        int[][] obstacles = new int[][]{};
+
+        assertEquals(9, QueensAttackII.queensAttack(4, obstacles.length, 1, 1, obstacles));
+
+    }
+
+    @Test
+    public void teste12() {
+        int[][] obstacles = new int[][]{{3, 3}};
+
+        assertEquals(7, QueensAttackII.queensAttack(4, obstacles.length, 1, 1, obstacles));
+
+    }
+
+    @Test
+    public void teste13() {
+        int[][] obstacles = new int[][]{{4, 4}};
+
+        assertEquals(8, QueensAttackII.queensAttack(4, obstacles.length, 1, 1, obstacles));
+
+    }
+
+    @Test
+    public void teste14() {
+        int[][] obstacles = new int[][]{};
+
+        assertEquals(9, QueensAttackII.queensAttack(4, obstacles.length, 4, 4, obstacles));
+
+    }
+
+    @Test
+    public void teste15() {
+        int[][] obstacles = new int[][]{{2, 2}};
+
+        assertEquals(7, QueensAttackII.queensAttack(4, obstacles.length, 4, 4, obstacles));
+
+    }
+
+    @Test
+    public void teste16() {
+        int[][] obstacles = new int[][]{};
+
+        assertEquals(9, QueensAttackII.queensAttack(4, obstacles.length, 4, 1, obstacles));
+
+    }
+
+    @Test
+    public void teste17() {
+        int[][] obstacles = new int[][]{{3, 2}};
+
+        assertEquals(7, QueensAttackII.queensAttack(4, obstacles.length, 1, 4, obstacles));
+
+    }
+
+    @Test
+    public void testeMaximus() {
+
+        int[][] obstacles = new int[][]{
+                {54, 87},
+                {64, 97},
+                {42, 75},
+                {32, 65},
+                {42, 87},
+                {32, 97},
+                {54, 75},
+                {64, 65},
+                {48, 87},
+                {48, 75},
+                {54, 81},
+                {42, 81},
+                {45, 17},
+                {14, 24},
+                {35, 15},
+                {95, 64},
+                {63, 87},
+                {25, 72},
+                {71, 38},
+                {96, 97},
+                {16, 30},
+                {60, 34},
+                {31, 67},
+                {26, 82},
+                {20, 93},
+                {81, 38},
+                {51, 94},
+                {75, 41},
+                {79, 84},
+                {79, 65},
+                {76, 80},
+                {52, 87},
+                {81, 54},
+                {89, 52},
+                {20, 31},
+                {10, 41},
+                {32, 73},
+                {83, 98},
+                {87, 61},
+                {82, 52},
+                {80, 64},
+                {82, 46},
+                {49, 21},
+                {73, 86},
+                {37, 70},
+                {43, 12},
+                {94, 28},
+                {10, 93},
+                {52, 25},
+                {50, 61},
+                {52, 68},
+                {52, 23},
+                {60, 91},
+                {79, 17},
+                {93, 82},
+                {12, 18},
+                {75, 64},
+                {69, 69},
+                {94, 74},
+                {61, 61},
+                {46, 57},
+                {67, 45},
+                {96, 64},
+                {83, 89},
+                {58, 87},
+                {76, 53},
+                {79, 21},
+                {94, 70},
+                {16, 10},
+                {50, 82},
+                {92, 20},
+                {40, 51},
+                {49, 28},
+                {51, 82},
+                {35, 16},
+                {15, 86},
+                {78, 89},
+                {41, 98},
+                {70, 46},
+                {79, 79},
+                {24, 40},
+                {91, 13},
+                {59, 73},
+                {35, 32},
+                {40, 31},
+                {14, 31},
+                {71, 35},
+                {96, 18},
+                {27, 39},
+                {28, 38},
+                {41, 36},
+                {31, 63},
+                {52, 48},
+                {81, 25},
+                {49, 90},
+                {32, 65},
+                {25, 45},
+                {63, 94},
+                {89, 50},
+                {43, 41},
+
+        };
+
+        assertEquals(40, QueensAttackII.queensAttack(100, 100, 48, 81, obstacles));
+    }
+
 
 }
