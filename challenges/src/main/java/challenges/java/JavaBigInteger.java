a28444b8ff063f98d0fd1af7e301a18d2f3eaf99
Leonardo Meinerz Ramos <leonardo.meinerzramos28@gmail.com>
2019-12-10 07:49:38 -0300

    resolução minor questions

---
+++ challenges/src/main/java/challenges/java/JavaBigInteger.java
@@ -0,0 +1,17 @@
+package challenges.java;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class JavaBigInteger {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BigInteger a = new BigInteger(scanner.nextLine());
+        BigInteger b = new BigInteger(scanner.nextLine());
+
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+
+    }
+}
---
+++ challenges/src/main/java/challenges/java/JavaMd5.java
@@ -0,0 +1,22 @@
+package challenges.java;
+
+import javax.xml.bind.DatatypeConverter;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
+public class JavaMd5 {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+
+        MessageDigest digest = MessageDigest.getInstance("MD5");
+
+        byte[] hash = digest.digest(input.getBytes("UTF-8"));
+
+        System.out.println(DatatypeConverter.printHexBinary(hash).toLowerCase());
+
+    }
+}
